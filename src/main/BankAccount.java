905b1b9ef6926b3a2e23f230490b0131c31cd492
frankjlin16 <flin30@asu.edu>
2021-04-14 20:38:03 -0700

    Initial commit

---
+++ src/main/BankAccount.java
@@ -0,0 +1,15 @@
+/*
+CSC 205AC #37531
+Program 2: Inheritance and Polymorphism
+Author: Frank Lin 36680188
+Description: <of the file contents>
+ */
+
+package main;
+
+public class BankAccount {
+
+    //data
+    private int balance, credit, debit, fees; //in terms of pennies
+
+}
