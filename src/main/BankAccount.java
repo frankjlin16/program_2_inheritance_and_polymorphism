1dac4ae6df83609764a58cd0b883fe045a312196
frankjlin16 <flin30@asu.edu>
2021-04-14 21:27:17 -0700

    Added comments.

--- src/main/BankAccount.java
+++ src/main/BankAccount.java
@@ -2,7 +2,7 @@
 CSC 205AC #37531
 Program 2: Inheritance and Polymorphism
 Authors: Frank Lin 36680188 && todo: Trevor add your name and ID here
-Description: <of the file contents>
+Description: Simulate the inner workings of a bank account.
  */
 
 package main;
@@ -11,15 +11,19 @@ public abstract class BankAccount {
 
     //data
     private String accountNumber;
-    private int balance; //in terms of pennies
+    private int balance, credit, debit, fees; //in terms of pennies
     private double interestRate;
 
     //todo: figure out WTF this is suppose to do
     boolean credit(int amount) {
+        balance += amount; //todo: is this what requirements mean?
         return true;
     }
 
     //todo: figure out WTF this is suppose to do
+    //debit amounts will be subtracted from the balance
+    //will return false if the transaction cannot be made because of insufficient balance or insufficient credit limit.
+    //Otherwise they will return true.
     abstract boolean debit(int amount);
 
     //returns balance of this account
@@ -48,6 +52,7 @@ public abstract class BankAccount {
     }
 
     //applies interest rate -> balance + (balance * interestRate)
+    //will not be applied to any Savings or Checking account with a balance of zero or less
     abstract void applyInterest();
 
     //return information about this account in String form
